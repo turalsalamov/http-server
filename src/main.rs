d8f8727c06144b3273061f4d8263b974c8b65f05
Tural Salamov <turalsalamov109@gmail.com>
2022-07-23 02:25:06 +0400

    We got incoming request

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,29 @@ use std::io::{ stdin, stdout, Write };
 use std::net::{ TcpListener };
 fn main() {
 
+    // Getting a desired port number from user
     print!("Please specify the port number: ");
-    stdout().flush().ok().expect("There is a problem in flush!");
+    stdout().flush().ok().expect("There is a problem in flush!"); // flushing the stdout
     let mut port: String = String::new();
-
     stdin().read_line(&mut port)
                                     .ok()
                                     .expect("There is an error in input!");
     
+    // Constructing the ip and port together
     let mut ip_and_port:String = String::from("127.0.0.1");
     ip_and_port.push(':');
-    ip_and_port.push_str(port.as_str());
+    ip_and_port.push_str(port.trim());
 
-    let listener = TcpListener::bind(&ip_and_port)
-                                    .ok()
-                                    .expect("There is a problem in binding the ip and port!");
-    match listener.accept() {
-        Ok((_socket, _addr)) => println!("Everything is ok"),
-        Err(_e) => println!("There is an error in accepting the requests"),
+    // Binding the listener to specified port for ip
+    let listener = TcpListener::bind(ip_and_port.as_str())
+                                            .ok()
+                                            .expect("There is a problem in binding the ip and port!");
+    // Accepting the requests
+    loop {
+        match listener.accept() {
+            Ok((_socket, _addr)) => println!("Everything is ok"),
+            Err(_e) => println!("There is an error in accepting the requests"),
+        }
     }
 
 }
